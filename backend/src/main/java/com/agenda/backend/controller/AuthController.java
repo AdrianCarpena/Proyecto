e7c104ba8e124024324930367fa35b77a7cc410e
Miguel <miguel.acosta.sotodosos@gmail.com>
2026-03-01 13:26:04 +0100

    DTO y nuevo Controller implementados

--- backend/src/main/java/com/agenda/backend/controller/AuthController.java
+++ backend/src/main/java/com/agenda/backend/controller/AuthController.java
@@ -1,5 +1,39 @@
 package com.agenda.backend.controller;
 
+import com.agenda.backend.dto.AuthRequest;
+import com.agenda.backend.model.User;
+import com.agenda.backend.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
 
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody AuthRequest request) {
+        try {
+            User user = authService.register(request.getUsername(), request.getPassword());
+            return ResponseEntity.ok("Usuario registrado correctamente");
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody AuthRequest request) {
+        try {
+            String token = authService.login(request.getUsername(), request.getPassword());
+            return ResponseEntity.ok(token);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(e.getMessage());
+        }
+    }
 }
--- backend/src/main/java/com/agenda/backend/model/User.java
+++ backend/src/main/java/com/agenda/backend/model/User.java
@@ -1,5 +1,51 @@
 package com.agenda.backend.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+//Indicamos que es una entidad JPA
+@Entity
+//Nombre que tendrá la tabla
+@Table(name = "users")
+//Generar constructor vacio automáticamente sin código
+@NoArgsConstructor
+//Generar constructor con todos los parámetros sin código
+@AllArgsConstructor
+//Anotación para poder instanciar entidades con .build (más comodo)
+@Builder
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
