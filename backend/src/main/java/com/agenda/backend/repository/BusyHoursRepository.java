5c4698939d1c7db525a3cb70193d31662a9652e6
AdrianC <adrian96482427@gmail.com>
2026-03-22 18:35:56 +0100

    Add files via upload

---
+++ backend/src/main/java/com/agenda/backend/repository/BusyHoursRepository.java
@@ -0,0 +1,16 @@
+package com.agenda.backend.repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.agenda.backend.model.BusyHours;
+import com.agenda.backend.model.User;
+
+public interface BusyHoursRepository extends JpaRepository<BusyHours, Long> {
+
+    List<BusyHours> findByUser(User user);
+    
+    List<BusyHours> findByUserAndFecha(User user, LocalDate fecha);
+  
+}
---
+++ backend/src/main/java/com/agenda/backend/repository/ExamenRepository.java
@@ -0,0 +1,15 @@
+package com.agenda.backend.repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.agenda.backend.model.Examen;
+import com.agenda.backend.model.User;
+
+public interface ExamenRepository extends JpaRepository<Examen, Long> {
+
+    List<Examen> findByUser(User user);
+    
+    List<Examen> findByUserAndFechaAfter(User user, LocalDate fecha);
+}
